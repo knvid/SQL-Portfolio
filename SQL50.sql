86fba1b2ba4fcb2d30970306f4f65fc574da6277
knvid <knvidhya@outlook.com>
2025-02-05 09:28:20 -0500

    practice from 2/4/25

--- SQL50.sql
+++ SQL50.sql
@@ -1,3 +1,5 @@
+-- LeetCode SQL50 Practice Problems
+
 -----------------------------------------------------------------------------------------------------------------------------
 
 -- 1757. Recyclable and Low Fat Products
@@ -95,6 +97,172 @@ WHERE area >= 3000000 OR population >= 25000000;
 
 -- Return the result table sorted by id in ascending order.
 
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id ASC;
 
+-----------------------------------------------------------------------------------------------------------------------------
+
+-- 1683. Invalid Tweets
+
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | tweet_id       | int     |
+-- | content        | varchar |
+-- +----------------+---------+
+-- tweet_id is the primary key (column with unique values) for this table.
+-- content consists of characters on an American Keyboard, and no other special characters.
+-- This table contains all the tweets in a social media app.
+ 
+
+-- Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+-- Return the result table in any order.
+
+SELECT tweet_id
+FROM Tweets
+WHERE LENGTH(content) > 15;
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+-- 1378. Replace Employee ID With The Unique Identifier
+
+-- Table: Employees
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains the id and the name of an employee in a company.
+
+-- Table: EmployeeUNI
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | unique_id     | int     |
+-- +---------------+---------+
+-- (id, unique_id) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table contains the id and the corresponding unique id of an employee in the company.
+ 
+
+-- Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+-- Return the result table in any order.
+
+SELECT EmployeeUNI.unique_id, Employees.name
+FROM Employees LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id;
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+-- 1068. Product Sales Analysis I
+
+-- Table: Sales
+-- +-------------+-------+
+-- | Column Name | Type  |
+-- +-------------+-------+
+-- | sale_id     | int   |
+-- | product_id  | int   |
+-- | year        | int   |
+-- | quantity    | int   |
+-- | price       | int   |
+-- +-------------+-------+
+-- (sale_id, year) is the primary key (combination of columns with unique values) of this table.
+-- product_id is a foreign key (reference column) to Product table.
+-- Each row of this table shows a sale on the product product_id in a certain year.
+-- Note that the price is per unit.
+ 
+
+-- Table: Product
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | product_id   | int     |
+-- | product_name | varchar |
+-- +--------------+---------+
+-- product_id is the primary key (column with unique values) of this table.
+-- Each row of this table indicates the product name of each product.
+ 
+
+-- Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+
+-- Return the resulting table in any order.
+
+SELECT Product.product_name, Sales.year, Sales.price
+FROM Sales LEFT JOIN Product ON Sales.product_id = Product.product_id;
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+
+-- Table: Visits
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | visit_id    | int     |
+-- | customer_id | int     |
+-- +-------------+---------+
+-- visit_id is the column with unique values for this table.
+-- This table contains information about the customers who visited the mall.
+ 
+
+-- Table: Transactions
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | transaction_id | int     |
+-- | visit_id       | int     |
+-- | amount         | int     |
+-- +----------------+---------+
+-- transaction_id is column with unique values for this table.
+-- This table contains information about the transactions made during the visit_id.
+ 
+
+-- Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+-- Return the result table sorted in any order.
+
+SELECT Visits.customer_id, COUNT(Visits.visit_id) AS count_no_trans
+FROM Visits
+LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id
+WHERE Transactions.transaction_id IS NULL
+GROUP BY Visits.customer_id;
+
+-- with aliasing
+
+SELECT V.customer_id, COUNT(V.visit_id) AS count_no_trans
+FROM Visits AS V
+LEFT JOIN Transactions AS T ON V.visit_id = T.visit_id
+WHERE T.transaction_id IS NULL
+GROUP BY V.customer_id;
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+-- 197. Rising Temperature
+
+-- Table: Weather
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | recordDate    | date    |
+-- | temperature   | int     |
+-- +---------------+---------+
+-- id is the column with unique values for this table.
+-- There are no different rows with the same recordDate.
+-- This table contains information about the temperature on a certain day.
+ 
+
+-- Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+-- Return the result table in any order.
+
+SELECT id
+FROM Weather
+WHERE temperature > 
 
 -----------------------------------------------------------------------------------------------------------------------------
